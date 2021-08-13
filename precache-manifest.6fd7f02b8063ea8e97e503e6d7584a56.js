dbbb1625845e6cdd24c6f0cebafd540945148d0d
jabaere <58694900+jabaere@users.noreply.github.com>
2021-08-13 20:06:48 +0400

    Add files via upload

---
+++ precache-manifest.6fd7f02b8063ea8e97e503e6d7584a56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f26ca41a06296ddd0915c741729ad812",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a3a6cc61ab7c7c2398c",
+    "url": "./static/css/main.7058f557.chunk.css"
+  },
+  {
+    "revision": "6997843cd631d9cbd447",
+    "url": "./static/js/2.7ab3c3ac.chunk.js"
+  },
+  {
+    "revision": "2a3a6cc61ab7c7c2398c",
+    "url": "./static/js/main.b3dcd648.chunk.js"
+  },
+  {
+    "revision": "1092f92989fb0f2e6624",
+    "url": "./static/js/runtime-main.4fc6b56b.js"
+  }
+]);
